657665b69a4bca2330b169fd225699fec251f3be
jose perez <joseperez@joses-MacBook-Pro.local>
2021-08-23 18:18:08 -0500

    uninstall 2 packages.

--- pages/index.js
+++ pages/index.js
@@ -7,38 +7,36 @@ import Footer from "../components/Footer/index"
 
 import styled from "styled-components";
 
-const DiscoverAisle = styled.div`
-    {
-      width:60%;
-      margin:auto;
-      @media (max-width: 1065px) {
-            width:100%;
-        }    }
-        `;
+const DiscoverAisle = styled.div`{
+  width:60%;
+  margin:auto;
+  @media (max-width: 1065px) {
+    width:100%;
+    }    
+  }`;
 
 // https://thecodest.co/blog/data-fetching-strategies-in-nextjs/
   // HTML will be generated after fetching the data by resolving the promise.
   // In this case, we can use the getStaticProps data fetching method
 export async function getStaticProps(context) {
-
   const res = await fetch("https://api.foxtrotchicago.com/v5/inventory/aisles/224/items?store_id=6")
   const products = await res.json()
-  // console.log(products);
 
   // if no data/error return error/nothing
   if (!products) {
       return {
           notFound: true,
-          
       }
   }
   return {
       props: { products },
   }
+  
 }
 
 const Index = ({ products }) => {
   const groups = products.aisle.groups;
+
 return (
   <div>
     <Head>
@@ -62,7 +60,7 @@ return (
                   </ul>
               </div>
           </DiscoverAisle>
-        <Footer></Footer>
+        <Footer />
     </main>
   </div>
 );
